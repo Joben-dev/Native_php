f04c2020388e43a39c2ad216531e5bbe6ec5608c
Joben-dev <jobendev6789@gmail.com>
2025-01-27 14:23:19 +0800

    update the index.php

--- index.php
+++ index.php
@@ -1,18 +1,21 @@
 <?php
 // index.php
 
-// Include the routes file
-include 'routes.php';
+// Get the current URI
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Define some routes
-get('/', function() {
-    return 'Hello, World!sss';
-});
+// Define routes and their corresponding responses
+$routes = [
+    '/' => 'Hello, World!',
+    '/about' => 'This is the About page!',
+    '/contact' => 'This is the Contact page!',
+];
 
-get('/about', function() {
-    return 'This is the About page!';
-});
-
-post('/submit', function() {
-    return 'Form submitted!';
-});
+// Check if the requested route exists
+if (array_key_exists($requestUri, $routes)) {
+    echo $routes[$requestUri];
+} else {
+    // If route does not exist, show a 404 message
+    http_response_code(404);
+    echo '404 Not Found';
+}
